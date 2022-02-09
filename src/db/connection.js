a738fa2a5a6815e20dbcbebbbdcfc94eaacce4dc
simonamarcu2 <supersimy16@gmail.com>
2022-02-09 12:10:00 +0000

    test1

--- src/db/connection.js
+++ src/db/connection.js
@@ -4,13 +4,14 @@ const { MongoClient } = require("mongodb");
 const client = new MongoClient(process.env.MONGO_URI);
 
 const connection = async () => {
-    try{
+    try {
         await client.connect();
         const db = client.db("Movies");
-        return db.collection ("Movie");
+        return db.collection("Movie");
     } catch (error) {
         console.log(error)
     }
 }
 
-module.exports = {client,connection};
+module.exports = { client, connection };
+//change
